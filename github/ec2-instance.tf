a80d21e9d788c4a61fc7c4f3ad23e9c7f2a86309
krishnaadale <krishnadale@gmail.com>
2024-08-01 00:14:47 +0530

    add

---
+++ github/ec2-instance.tf
@@ -0,0 +1,53 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.60.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-l"
+
+}
+# Create a security group
+resource "aws_security_group" "example_sg" {
+  name        = "terraform-example-sg"
+  description = "Security group for Terraform example"
+  vpc_id      = "vpc-0ed19d3f73f808d94"  # Replace with your VPC ID
+
+  # Allow incoming SSH traffic
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "terraform-example-sg"
+  }
+}
+
+# Create an EC2 instance
+resource "aws_instance" "example_instance" {
+  ami           = "ami-068e0f1a600cd311c"  # Replace with your preferred AMI ID
+  instance_type = "t2.micro"
+  key_name      = "krishna.pem"  # Replace with your key pair name
+
+  # Associate the security group
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
+
+  tags = {
+    Name = "terraform-example-instance"
+  }
+}
