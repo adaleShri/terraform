a727d281a01ad849b7a111b5166073e14f5eacef
adaleShri <adaleshree@gmail.com>
2026-02-05 23:21:43 +0530

    Update ec2_ins.tf

--- ec2_ins.tf
+++ ec2_ins.tf
@@ -1,29 +1,32 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.31.0"
     }
   }
 }
 
 provider "aws" {
- region = "ap-south-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "test_instance" {
-    ami = "ami-019715e0d74f695be"
-    instance_type = "t3.micro"
-    key_name = "ne-ac-key"
-    tags = {
-        name = "terrafrom created by ec2"
- }
- output "instance_id" {
-   value = "aws_instance.test_instance.id"
- }
- output "public_id"{
-    value = "aws_instance.test.public.id"
- }
- 
+  ami           = "ami-019715e0d74f695be"
+  instance_type = "t3.micro"
+  key_name      = "ne-ac-key"   # <-- NO .pem file
 
+  tags = {
+    Name = "terraform-created-ec2"
+  }
+}
+
+# -------- OUTPUTS MUST BE OUTSIDE THE RESOURCE --------
+
+output "instance_id" {
+  value = aws_instance.test_instance.id
+}
+
+output "public_ip" {
+  value = aws_instance.test_instance.public_ip
 }
